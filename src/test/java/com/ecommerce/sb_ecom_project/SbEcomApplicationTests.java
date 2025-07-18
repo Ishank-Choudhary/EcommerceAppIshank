dcb9d67bc4b6c8668fb6db3c694ee94c26d97ce6
Ishank Choudhary <54226187+Ishank-Choudhary@users.noreply.github.com>
2025-07-19 01:02:01 +0530

    adding project to git

---
+++ src/test/java/com/ecommerce/sb_ecom_project/SbEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sb_ecom_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
