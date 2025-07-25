c31cd0781d3092224b7986cae79606e5666e0ddb
Ishank Choudhary <54226187+Ishank-Choudhary@users.noreply.github.com>
2025-07-26 04:04:47 +0530

    ECOM-adding delete by categoryId

--- src/main/java/com/ecommerce/sb_ecom_project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sb_ecom_project/controller/CategoryController.java
@@ -2,10 +2,8 @@ package com.ecommerce.sb_ecom_project.controller;
 
 import com.ecommerce.sb_ecom_project.model.Category;
 import com.ecommerce.sb_ecom_project.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,10 +22,17 @@ public class CategoryController {
     public List<Category> getAllCategories(){
         return categoryService.getAllCategories();
     }
+
     @PostMapping("/api/public/categories")
     public String createCategory(@RequestBody Category category){
         categoryService.createCategory(category);
         return "Category added successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable String categoryId){
+        categoryService.deleteCategory(categoryId);
+        return "Category removed successfully";
     }
 
 }
